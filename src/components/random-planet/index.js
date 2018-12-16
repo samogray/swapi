23d3ab40e2138cc1fa1f354c13e3cd81b5f773b7
Oleksandr Samohrai <sam_gray@ukr.net>
2018-12-16 17:02:07 +0200

    loading

--- src/components/random-planet/index.js
+++ src/components/random-planet/index.js
@@ -11,14 +11,16 @@ import {
   Col
 } from 'reactstrap'
 
+import Spinner from '../spinner'
+
 import SwapiService from '../../utils/swapi-service'
 
 class RandomPlanet extends React.Component {
-
   swapiService = new SwapiService()
 
   state = {
-   planet: {}
+    planet: {},
+    loading: true,
   }
 
   componentDidMount() {
@@ -26,51 +28,59 @@ class RandomPlanet extends React.Component {
     this.updatePlanet(id)
   }
 
-  onPlanetLoaded = (planet) => this.setState({planet})
+  onPlanetLoaded = planet => this.setState({planet, loading: false})
 
-  updatePlanet = (id) => this.swapiService.getPlanet(id)
-  .then(this.onPlanetLoaded)
-  .catch(err => console.error(err))
-
+  updatePlanet = id =>
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(err => console.error(err))
 
   render() {
-    const {population, name, rotation, diameter, pic} = this.state.planet
+
+    const {
+      population,
+      name,
+      rotation,
+      diameter,
+      pic
+    } = this.state.planet
+
     return (
       <div>
         <Card>
           <Container>
-            <Row>
-              <Col xs="3">
-                <CardImg
-                  top
-                  width="100%"
-                  src={pic}
-                  alt={name}
-                />
-              </Col>
-              <Col xs="8">
-                <CardBody>
-                  <CardTitle>
-                    <span>Name: </span>
-                    <span>{name}</span>
-                  </CardTitle>
-                  <ListGroup>
-                    <ListGroupItem>
-                      <span>Population: </span>
-                      <span>{population}</span>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <span>Rotation Period :</span>
-                      <span>{rotation}</span>
-                    </ListGroupItem>
-                    <ListGroupItem>
-                      <span>Diameter :</span>
-                      <span>{diameter}</span>
-                    </ListGroupItem>
-                  </ListGroup>
-                </CardBody>
-              </Col>
-            </Row>
+            {!this.state.loading ? (
+              <Row>
+                <Col xs="3">
+                  <CardImg top width="100%" src={pic} alt={name} />
+                </Col>
+                <Col xs="8">
+                  <CardBody>
+                    <CardTitle>
+                      <span>Name: </span>
+                      <span>{name}</span>
+                    </CardTitle>
+                    <ListGroup>
+                      <ListGroupItem>
+                        <span>Population: </span>
+                        <span>{population}</span>
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        <span>Rotation Period :</span>
+                        <span>{rotation}</span>
+                      </ListGroupItem>
+                      <ListGroupItem>
+                        <span>Diameter :</span>
+                        <span>{diameter}</span>
+                      </ListGroupItem>
+                    </ListGroup>
+                  </CardBody>
+                </Col>
+              </Row>
+            ) : (
+              <Spinner />
+            )}
           </Container>
         </Card>
       </div>
